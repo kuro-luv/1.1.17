2214877f82dd839953150b5deb6b6c125ee40ac2
kuro-luv <kuro.luw@gmail.com>
2026-03-17 21:15:13 +0300

    init com

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+
+const App = () => {
+    return (
+        <div>
+            <div className='testPic'></div>
+            <p className='testText'>test cat</p>
+        </div>
+    )
+}
+
+export default App

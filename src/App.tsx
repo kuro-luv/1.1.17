3ab28fe6a7b3a9945e3f1d8afd7a54c3d8cb5cad
kuro-luv <kuro.luw@gmail.com>
2026-03-18 13:26:50 +0300

    cosmetic fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.scss'
 const App = () => {
     return (
         <div>
-            <div className='test-pic'></div>
+            <div className='test-pic' />
             <p className='test-text'>test cat</p>
         </div>
     )
